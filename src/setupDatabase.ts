997e735226a3043bc3061dcc5a1ef238aa803573
Abdul Rehman Aziz <42958150+abdulrehman1363@users.noreply.github.com>
2024-08-22 00:19:55 +0500

    setup project

---
+++ src/setupDatabase.ts
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+import { config } from './config';
+import Logger from 'bunyan';
+
+const log: Logger = config.createLogger('Database');
+
+export default () => {
+  const connect = () => {
+    mongoose
+      .connect(`${config.DATABASE_URL}`)
+      .then(() => {
+        log.info('Database connection established');
+      })
+      .catch((error) => {
+        log.error('Database connection error ' + error);
+        return process.exit(1);
+      });
+  };
+
+  connect();
+  mongoose.connection.on('disconnected', connect);
+};
